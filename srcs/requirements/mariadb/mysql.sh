2d074e293d3d1e28ebd15cf0248bbfdd4634e585
abel-hid <abdeljalilelhidaoui21@gmail.com>
2024-04-28 10:25:25 -0400

    edit

--- srcs/requirements/mariadb/mysql.sh
+++ srcs/requirements/mariadb/mysql.sh
@@ -6,15 +6,20 @@ service mysql start
 # Wait for MariaDB to start
 sleep 5
 
-# Create database and user
-mysql -e "CREATE DATABASE wordpress;"
-mysql -e "CREATE USER 'abel-hid'@'%' IDENTIFIED BY 'abel-hid';"
-mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'abel-hid'@'%';"
-mysql -e "FLUSH PRIVILEGES;"
+# Set root password if not set
+if [ -z "$(mysql -u root -sse "SELECT user FROM mysql.user WHERE user='root'")" ]; then
+    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root1234';"
+fi
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root1234';"
+# Create database and user
+mysql -u root -p"root1234" -e "CREATE DATABASE IF NOT EXISTS wordpress;"
+mysql -u root -p"root1234" -e "CREATE USER IF NOT EXISTS 'abel-hid'@'%' IDENTIFIED BY 'abel-hid';"
+mysql -u root -p"root1234" -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'abel-hid'@'%';"
+mysql -u root -p"root1234" -e "FLUSH PRIVILEGES;"
+
 # Stop MariaDB service
 service mysql stop
 
 # Keep container running
+echo "MariaDB: Setup completed!"
 mysqld
