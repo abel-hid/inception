a72c4b9360d0e8854b4a49d124dc585ddd1c8d20
abel-hid <abdeljalilelhidaoui21@gmail.com>
2024-10-19 11:06:03 -0400

    edit

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -2,7 +2,9 @@
 
 
 if [ ! -f /etc/nginx/ssl/nginx.crt ]; then
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/C=FR/ST=Paris/L=Paris/O=42/OU=42/CN=abel-hid.42.fr";
-    echo "SSL certificate created";
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 
+        -keyout /etc/nginx/ssl/nginx.key
+        -out /etc/nginx/ssl/nginx.crt
+        -subj "/C=MA/ST=Casablanca/L=Casablanca/O=1337/OU=1337/CN=abel-hid.42.fr";
 fi
 nginx -g "daemon off;"
