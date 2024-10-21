5c054abb795b43b3a45c0003dceeaccf2bfe4104
Abdeljalil El Hidaoui <abdeljalilelhidaoui21@gmail.com>
2024-10-21 16:24:46 +0100

    edit

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -8,8 +8,6 @@ touch /run/php/php7.4-fpm.pid
 
 # Check if wp-config.php exists, if not, set up WordPress
 if [ ! -f /var/www/html/wp-config.php ]; then
-    echo "WordPress: Setting up..."
-    
     # Download WP-CLI and make it executable
     wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
@@ -25,16 +23,22 @@ if [ ! -f /var/www/html/wp-config.php ]; then
         
         # Generate wp-config.php file
         wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="$MYSQL_HOSTNAME" --dbprefix=wp_ --allow-root --force
+        
         # Install WordPress
         wp core install --allow-root --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_LOGIN" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL"
         
         # Create additional user (optional)
         wp user create --allow-root "$WP_USER_LOGIN" "$WP_USER_EMAIL" --user_pass="$WP_USER_PASSWORD"
-        
-        echo "WordPress: Setup completed!"
     else
         echo "WordPress: Files already present. Skipping setup."
     fi
+
+    # Set ownership of WordPress files to the web server user (www-data for Apache/Nginx)
+    chown -R www-data:www-data /var/www/html
+    
+    # Set the correct permissions for directories and files
+    find /var/www/html -type d -exec chmod 755 {} \;    # Directories: 755
+    find /var/www/html -type f -exec chmod 644 {} \;    # Files: 644
 fi
 
 # Start PHP-FPM
